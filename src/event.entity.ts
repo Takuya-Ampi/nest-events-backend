aea74a0859ebcb8416b75a1fb98aeaabe99f9ba4
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-10 06:49:03 +0900

    fix: repository pattern

--- src/event.entity.ts
+++ src/event.entity.ts
@@ -1,7 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
 export class Event {
+  @PrimaryGeneratedColumn()
   id: number
+  @Column({
+    type: "varchar",
+    unique: true,
+    nullable: false
+  })
+  @Column({length: 100})
   name: string
+  @Column()
   description: string
+  @Column()
   when: Date
+  @Column()
   address: string
 }
