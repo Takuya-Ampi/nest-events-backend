c1fd23748373c1c85ca68c029a8ca336feab5dd0
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-09 18:33:07 +0900

    fix: crud

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,29 +1,43 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 import { CreateEventDto } from "./create-event.dto";
+import { Event } from "./event.entity";
 import { UpdateEventDto } from "./update-event.dto";
 
 @Controller('events')
 export class EventsController {
+  private events: Event[] = []
   @Get()
   findAll() {
-    return [
-      { id: 1, name: 'First event' },
-      { id: 2, name: 'Second event' }
-    ]
+    return this.events
   }
   @Get(':id')
   findOne(@Param('id') id) {
-    return { id: 1, name: 'First event' }
+    const event = this.events.find(event => event.id === Number(id))
+    return event
   }
   @Post()
   create(@Body() input: CreateEventDto) {
-    return input
+    const event = {
+      ...input,
+      when: new Date(input.when),
+      id: this.events.length + 1
+    }
+    this.events.push(event)
+    return event
   }
   @Patch(':id')
   update(@Param('id') id, @Body() input: UpdateEventDto) {
-    return input
+    const index = this.events.findIndex(event => event.id === Number(id))
+    this.events[index] = {
+      ...this.events[index],
+      ...input,
+      when: input.when ? new Date(input.when) : this.events[index].when
+    }
+    return this.events[index]
   }
   @Delete(':id')
   @HttpCode(204)
-  remove() {}
+  remove(@Param('id') id) {
+    this.events = this.events.filter(event => event.id !== Number(id))
+  }
 }
--- src/update-event.dto.ts
+++ src/update-event.dto.ts
@@ -2,5 +2,5 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateEventDto } from "./create-event.dto";
 import { NestedPartial } from './types/index'
 
-// export class UpdateEventDto extends NestedPartial(CreateEventDto) {}
-export type UpdateEventDto = NestedPartial<CreateEventDto>
+export class UpdateEventDto extends PartialType(CreateEventDto) {}
+// export type UpdateEventDto = NestedPartial<CreateEventDto>
