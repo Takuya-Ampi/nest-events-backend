32a0ad4f147b054de0de6b3e9b29cbdfc848c7c7
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-10 10:11:28 +0900

    fix: 途中

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { Like, MoreThan, Repository } from "typeorm";
 import { CreateEventDto } from "./create-event.dto";
 import { Event } from "./event.entity";
 import { UpdateEventDto } from "./update-event.dto";
@@ -11,7 +11,21 @@ export class EventsController {
     @InjectRepository(Event)
     private readonly repository: Repository<Event>
   ) {}
-  private events: Event[] = []
+
+  @Get('practice')
+  async practice() {
+    return await this.repository.find( {
+        where: [
+            { 
+              id: MoreThan(3),
+              when: MoreThan(new Date("2021-02-12T13:00:00"))
+            }, 
+            { description: Like('%meet%')}
+          ],
+        take: 2
+      }
+    )
+  }
   @Get()
   async findAll() {
     return await this.repository.find()
