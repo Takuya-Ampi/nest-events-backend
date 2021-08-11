25d5d7ab7be70b37ef1941d3ec4b8c71fea2d9da
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-11 10:01:13 +0900

    fix: event.module

--- src/events/update-event.dto.ts
+++ src/events/update-event.dto.ts
@@ -1,6 +1,6 @@
 import { PartialType } from "@nestjs/mapped-types";
 import { CreateEventDto } from "./create-event.dto";
-import { NestedPartial } from './types/index'
+import { NestedPartial } from '../types/index'
 
 export class UpdateEventDto extends PartialType(CreateEventDto) {}
 // export type UpdateEventDto = NestedPartial<CreateEventDto>
