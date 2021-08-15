bf05e9bbb60c85c37d67e4e27fbe02cd40c7a588
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-15 17:58:52 +0900

    fix: jwtのジェネレート、照合

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+
   @Post('login')
   @UseGuards(AuthGuard('local')) // local-strategyのvalidateが呼ばれ、userがreturnされる
   async login(@Request() request) { // UseGuardsによりrequest.userが取得できる
     return {
       userId: request.user.id,
-      token: 'the token will go here'
+      token: this.authService.getTokenForUser(request.user)
     }
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard('jwt'))
+  async getProfile(@Request() request) {
+    return request.user
   }
 }
