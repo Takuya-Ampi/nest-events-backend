a2d165220f7240f99483424a2a5ee87d4d2c5bc7
Takuya-Ampi <ap.takuya@gmail.com>
2021-08-19 09:34:39 +0900

    fix: test

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,7 +1,10 @@
+import { User } from "./../auth/user.entity"
 import { Repository } from "typeorm"
 import { Event } from "./event.entity"
 import { EventsController } from "./events.controller"
 import { EventsService } from "./events.service"
+import { ListEvents } from "./input/list.events"
+import { NotFoundException } from "@nestjs/common"
 
 describe('eventsController', () => {
   let eventsService: EventsService
@@ -9,15 +12,53 @@ describe('eventsController', () => {
   let eventsRepository: Repository<Event>
   // 1回だけ実行
   beforeAll(() => console.log('test start'))
+
+  // itの数だけ実行
   beforeEach(() => {
+    console.log('aaa count')
     eventsService = new EventsService(eventsRepository)
     eventsController = new EventsController(eventsService)
   })
 
-  // it('should return events list', async () => {
-  //   const n = null
-  //   expect(n).toBeNull()
-  // })
+  it('should return events list', async () => {
+    const result = {
+      first: 1,
+      last: 1,
+      limit: 10,
+      data: []
+    }
+
+    // eventServiceに直接代入してmock化する
+    // eventsService.getEventsWithAttendeeCountFilteredPaginated
+    //   = jest.fn().mockImplementation((): any => result)
+
+    // spyOn使ってmocka化する
+
+    const spy = jest
+      .spyOn(eventsService, 'getEventsWithAttendeeCountFilteredPaginated')
+      .mockImplementation((): any => result)
+
+
+    expect(await eventsController.findAll(new ListEvents))
+      .toEqual(result)
+
+    expect(spy).toBeCalledTimes(1)
+  })
+
+  it('should not delete an event, when it\'s not found', async () => {
+    const deleteSpy = jest.spyOn(eventsService, 'deleteEvent')
+    const findSpy = jest.spyOn(eventsService, 'findOne')
+    .mockImplementation((): any => undefined)
+
+    try {
+      await eventsController.remove(1, new User())
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException)
+    }
+
+    expect(deleteSpy).toBeCalledTimes(0)
+    expect(findSpy).toBeCalledTimes(1)
+  })
   // it('test1', async () => {
   //   const n = null
   //   expect(n).toBeNull()
